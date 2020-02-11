6636e490a720003660251a2b77434306997dbaff
dwn84 <50523550+dwn84@users.noreply.github.com>
2020-02-11 10:39:02 -0500

    Ejemplo de manejo de formularios

---
+++ wfAppManejoFormularios/frmConvertir.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class frmConvertir : Form
+
+       
+    {
+
+        private int x;
+
+        public int X { get => x; set => x = value; }
+
+        public frmConvertir()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            x = 88;
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            x = 99;
+        }
+    }
+}
---
+++ wfAppManejoFormularios/frmMenu.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class frmMenu : Form
+    {
+        public frmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void tsmConvertir_Click(object sender, EventArgs e)
+        {
+            bool isOpen = false;
+
+            foreach (Form f in Application.OpenForms) 
+            {
+
+                if (f.Text == "Convertir")
+                {
+                    isOpen = true;
+                    f.Focus();
+                    break;
+                }
+                
+            }
+
+            if (!isOpen)
+            {
+                var myForm = new frmConvertir();
+                myForm.MdiParent = this;
+                myForm.Show();
+
+
+            }
+
+
+        }
+
+        private void tsmIndiceMasaCorporal_Click(object sender, EventArgs e)
+        {
+            bool isOpen = false;
+
+
+            foreach (Form f in Application.OpenForms)
+            {
+                
+                if (f.Text == "Indice de Masa Corporal")
+                {
+                    isOpen = true;
+                    f.Focus();
+                    break;
+                }
+
+            }
+
+            if (!isOpen)
+            {
+                var myForm = new frmIndiceMasaCorporal();
+                myForm.MdiParent = this;
+                myForm.Show();
+
+
+            }
+
+        }
+
+        private void tsmCalcularNota_Click(object sender, EventArgs e)
+        {
+            bool isOpen = false;
+
+            foreach (Form f in Application.OpenForms)
+            {
+
+                if (f.Text == "Calcular nota final")
+                {
+                    isOpen = true;
+                    f.Focus();
+                    break;
+                }
+
+            }
+
+            if (!isOpen)
+            {
+                var myForm = new frmCalcularNotaFinal();
+                myForm.MdiParent = this;
+                myForm.Show();
+                
+            }
+        }
+
+        private void tsmSimuladorBanco_Click(object sender, EventArgs e)
+        {
+            bool isOpen = false;
+
+            foreach (Form f in Application.OpenForms)
+            {
+
+                if (f.Text == "Simulador Bancario")
+                {
+                    isOpen = true;
+                    f.Focus();
+                    break;
+                }
+
+            }
+
+            if (!isOpen)
+            {
+                var myForm = new frmSimuladorBancario();
+                myForm.MdiParent = this;
+                myForm.Show();
+
+            }
+        }
+    }
+}
